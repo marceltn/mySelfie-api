ae68e855e3a179d4d997b3df32febb4985c51168
Alberto Rocha <betogrun@gmail.com>
2016-04-16 18:58:20 -0300

    Add User entity

---
+++ api/controllers/UserController.js
@@ -0,0 +1,15 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+	login: function(req,res){
+		res.send("Hello");
+		console.log('hello');
+	}
+};
+
